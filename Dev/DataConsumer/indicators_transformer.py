040b0111ffbbfe024c67f3cdcbd6f4da8b9f7bd1
PAEZ ORTEGA OSWALDO EMMANUEL <46234487+Osw1997@users.noreply.github.com>
2023-02-17 05:02:13 -0600

    Advances

--- Dev/DataConsumer/indicators_transformer.py
+++ Dev/DataConsumer/indicators_transformer.py
@@ -1,4 +1,3 @@
-from fileinput import filename
 import pandas as pd
 
 # Let's convert pivot table into a reguilar table for all the interesting indicators
@@ -12,18 +11,19 @@ for indicator in indicators:
     for file in indicators[indicator]:
         df = pd.read_csv(f"../../data/resources/{indicator}/{file}")
         # Make the magic!
+        column_value = f"value_{file.split('.')[0]}"
         melted_df = df.melt(
             id_vars=["ISO3", "Name"],
             var_name="Year",
-            value_name="value"
+            value_name=column_value
         )
         # Create a flag that indicates if value is real or mean
-        melted_df["no_value"] = melted_df["value"].isna()
+        melted_df[f"no_{column_value}"] = melted_df[column_value].isna()
         # Get mean by year and replace nan values by mean
-        mean_df = melted_df.groupby(["Year"])[["value"]].mean().reset_index()
-        mean_df.rename(columns={"value": "mean"}, inplace=True)
+        mean_df = melted_df.groupby(["Year"])[[column_value]].mean().reset_index()
+        mean_df.rename(columns={column_value: "mean"}, inplace=True)
         final_df = pd.merge(melted_df, mean_df, how="left", on="Year")
-        final_df.loc[final_df["value"].isna(), "value"] = final_df["mean"]
+        final_df.loc[final_df[column_value].isna(), column_value] = final_df["mean"]
         del final_df["mean"]
         # Save result to CSV
         melted_filename = f"melted_{file}"
